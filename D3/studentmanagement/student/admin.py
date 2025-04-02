88b220fa6445bf2ba97bb89f1ee8e761b0b55de6
ID-755 <023neb755@sxc.edu.np>
2025-04-02 12:22:13 +0545

    q

--- D3/studentmanagement/student/admin.py
+++ D3/studentmanagement/student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from student.models import Student, Course, Enrollment
+
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Enrollment)
